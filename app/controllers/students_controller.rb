e20c2d8091a5cf9dc1bd1da246d7ad885bdeaf01
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-16 13:00:37 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,15 +3,17 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
-
-  def show
-    @student = Student.find(params[:id])
-  end
-
+  
   def new
   end
 
   def create
+    student = Student.new(:first_name => params["first_name"], :last_name =>  params["last_name"])
+    redirect_to student_path(student) if !!student.save
+  end
+
+  def show
+    @student = Student.find_by(:id => params["id"])
   end
 
 end
